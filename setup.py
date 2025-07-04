d8a4088556bad559fe0a2a48a3bf20f6f00212e2
rmcesarjr <96126126+rmcesarjr@users.noreply.github.com>
2025-07-04 13:21:29 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='0.1',
     description='Biblioteca educacional para o curso ICCD do IME-USP',
     author='Roberto Marcondes Cesar Junior',
-    author_email='rmcesar@usp.br',
+    author_email='seu-email@usp.br',
     url='https://github.com/rmcesarjr/iccd',
-    packages=find_packages(),
+    packages=find_packages(),  # <- ESSENCIAL para incluir iccd/
     install_requires=[
         'pandas'
     ],
